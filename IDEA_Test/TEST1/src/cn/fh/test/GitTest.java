4627d890bee286a3d71f2d1ae361ff7757c0c7ce
fh <xgaifh@163.com>
2020-02-15 00:16:20 +0800

    GitTest类中新增一个add方法

--- IDEA_Test/TEST1/src/cn/fh/test/GitTest.java
+++ IDEA_Test/TEST1/src/cn/fh/test/GitTest.java
@@ -8,4 +8,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello,GitHub!");
     }
+    public void add(){
+        System.out.println("新增一个方法");
+    }
 }
