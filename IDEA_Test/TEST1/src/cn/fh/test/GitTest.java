89d4e226deadf22bb2edcf2ab7638f9c09052e7b
fh <xgaifh@163.com>
2020-02-15 00:14:07 +0800

    将IDEA_Test项目提交到本地仓库

---
+++ IDEA_Test/TEST1/src/cn/fh/test/GitTest.java
@@ -0,0 +1,11 @@
+package cn.fh.test;
+
+/**
+ * @author FH
+ * @create 2020-02-15 0:07
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello,GitHub!");
+    }
+}
